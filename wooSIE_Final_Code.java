8d3099ced1b4e706f0502fead75bb9a859dfc588
tlepage0105 <55330628+tlepage0105@users.noreply.github.com>
2022-08-23 14:01:38 -0700

    Add files via upload

--- wooSIE_Final_Code.java
+++ wooSIE_Final_Code.java
@@ -14,7 +14,7 @@ import javax.swing.JPanel;
 
 public class final_Code {
 
-	
+	//Variables to be used throughout all functions
 	public static VNALibrary lib;
 	public static VNACalibratedSampleBlock rc;
 	public static boolean loop_Check = true;
@@ -27,6 +27,7 @@ public class final_Code {
 	public static MidiDevice device;
 	public static Receiver rcvr;
 	
+	//Main function
 	public static void main(String[] args) throws  InvalidMidiDataException, MidiUnavailableException {
 		connectToVNA();
 		getTheReceiver();
@@ -43,10 +44,12 @@ public class final_Code {
 		device.close();
 	}
 	
+	//Connect to VNA
 	public static void connectToVNA() {
 		lib = null;
 		rc = null;
 		
+		//Throw an error if there is a connection error
 		try {
 			lib = new VNALibrary();
 			lib.loadDriverByName("miniVNA Tiny", "COM10");
@@ -57,16 +60,25 @@ public class final_Code {
 		
 	}
 	
+	//Run the transmission test
 	public static void runTransmissionTestlib() {
 		
 		try {
+		  //////This needs to be updated based on users settings///////
 		  lib.loadCalibrationFile("C:\\Users\\tlepa\\vnaJ.3.4\\calibration\\Tran_Cal_220801_New.cal");  
+		  /////////////////////////////////////////////////////////////
+		  
+		  ////////These values are frequencies in Hertz to sweep through/////
 		  rc = lib.scan(1300000000, 1700000000, 10, "TRAN");
+		  //////////////////////////////////////////////////////////////////
+		  
+		  //Get the transmission loss of each frequency
 		  transLoss = new float[rc.getCalibratedSamples().length];
 		  for (int i = 0; i < (rc.getCalibratedSamples().length - 1);i++) {
 			  float data = (float)rc.getCalibratedSamples()[i].getTransmissionLoss();
 			  transLoss[i] = data;
 		  }
+		  //Find cumulative sum of the transmission loss
 		  float sum[] = cumulativeSum(transLoss);
 		  cumSum = sum[rc.getCalibratedSamples().length-1];
 		  System.out.println(cumSum);
@@ -81,34 +93,32 @@ public class final_Code {
 		int midi_low = 0;
 		int midi_high = 127;
 		
+		//Mapping of the midi values to the cumulative sum values
 		float slope = (midi_high - midi_low) / ((onCalValue) - offCalValue);
 		System.out.println(slope);
 		int midiPressure = (int) (midi_low + Math.round(slope * (cumSum - offCalValue)));
+		//Making sure the midid values do not go outside their range and throw an error
 		if (midiPressure > 127) {
 			midiPressure = 127;
-//			onCalValue = midiPressure;
 		}
 		else if (midiPressure < 0) {
 			midiPressure = 0;
 		}
 		System.out.println(midiPressure);
+		
+		//Creating the midi message to send
 		ShortMessage myMsg = new ShortMessage();
 		long timeStamp = -1;
 		myMsg.setMessage(176, 0, 50, midiPressure);
-//		sleep(2000);
 		rcvr.send(myMsg, timeStamp);
 	}
 	
+	//Button to stop testing
 	public static void CreateDialogFromOptionPane() {
         JFrame parent = new JFrame();
         JPanel panel = new JPanel();
-        JButton button_start = new JButton("Start Test");
         JButton button_stop = new JButton("Stop Test");
 
-//        Container contentPane = parent.getContentPane();	
-//        button_start.setText("Click to end test");
-//        button_stop.setText("Click to end test");
-        panel.add(button_start);
         panel.add(button_stop);
         parent.add(panel);
         parent.pack();
@@ -123,34 +133,21 @@ public class final_Code {
             }
         });
         
-        button_start.addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                parent.setVisible(false);
-                loop_Check = false;
-                
-            }
-        });
-        
     }
 	
+	//Button to find the calibration values for midi mapping
 	public static void collectInitialData() {
         JFrame parent = new JFrame();
         JPanel panel = new JPanel();
         JButton button_OffData = new JButton("Collect No Touch Calibration Data");
         JButton button_OnData = new JButton("Collect Touch Calibration Data ");
 
-//        Container contentPane = parent.getContentPane();	
-//        button_start.setText("Click to end test");
-//        button_stop.setText("Click to end test");
-//        JOptionPane.showMessageDialog(panel, "Click Here Once Done Calibrating");
         panel.add(button_OffData);
         panel.add(button_OnData);
         parent.add(panel);
         parent.pack();
         parent.setVisible(true);
-        
-//        JFrame stopFrame = new JFrame();
+        parent.setLocation(250,0);
 
         button_OffData.addActionListener(new java.awt.event.ActionListener() {
             @Override
@@ -183,18 +180,22 @@ public class final_Code {
         
     }
 	
+	//Finding the receiving device to send midi message to. For this application LoopBe was used
 	public static void getTheReceiver() throws MidiUnavailableException {
 		
 		MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
 		for(Info devices : infos )
 		{
+			///////This line will need to get updated if other devices are used.///////////
 			if(devices.getName().equals("2- LoopBe Internal MIDI") && devices.getDescription().equals("External MIDI Port")) {
+			//////////////////////////////////////////////////////////////////////////////	
 				device = MidiSystem.getMidiDevice(devices);
 				rcvr =  device.getReceiver();
 			}
 		}
 	}
 	
+	//Sleep function used for debugging
     public static void sleep(int time)
     {
         try {
